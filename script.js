b6bd66401ee25cf22973a5eb544f59230c3ca3c7
Agend047 <schneiderniklas@gmx.de>
2025-06-23 22:47:35 +0200

    Fixed: Lethals&Devastating are now accurat
    New functions, that make the calculations of Lethal hits and Devastating Woudns WAY more Accurate.
    Floor for Sustained Hits is also prepared with function "calculateOneOf6". Though it must be adapted for things like "Anti" or other things, that trigger below a rolled 6.

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ function checkFields(ident) {
             alert('Mit positiven Werten kommen realistischere Ergebnisse.')
         }
         if (document.getElementById('inAttack' + ident).value < 0) { document.getElementById('inAttack' + ident).value = 1 };
+        if (document.getElementById('inHitRoll' + ident).value <= 0) { document.getElementById('inHitRoll' + ident).value = 1 };
         calculate(ident);
     }
 }
@@ -36,31 +37,35 @@ function checkFields(ident) {
  * To always use the correct numbers for the questioned Calculation, we use a Number identificator.
  */
 function calculate(ident) {
-    let lethalHits = document.getElementById('lethalHitsCheck' + ident).checked;
-    let devastatingWounds = document.getElementById('devastatingWoundsCheck' + ident).checked;
+    let lethalHitsBool = document.getElementById('lethalHitsCheck' + ident).checked;
+    let devastatingWoundsBool = document.getElementById('devastatingWoundsCheck' + ident).checked;
     // let sustainedHitsX = document.getElementById('sustainedHitsNumber' + ident);
 
     let hits = Number(getHittingAttacks(ident));
     let additionalWounds = 0;
 
 
-    if (lethalHits) {
-        additionalWounds = hits * (1 / 6);
-        hits = hits * (5 / 6);
+    if (lethalHitsBool) {
+        additionalWounds = getLethalHits(hits, ident);
+        hits = hits - additionalWounds;
+        console.log("lethalHits: " + additionalWounds);
     }
 
     let woundingAttacks = Number(getWounding(hits, ident) + additionalWounds);
-    let mortalWounds = 0;
+    console.log("woundingAttacks: " + woundingAttacks);
 
-    if (devastatingWounds) {
-        mortalWounds = (woundingAttacks * (1 / 6)).toFixed(2);
-        woundingAttacks = woundingAttacks * (5 / 6);
+    let mortalWounds = 0;
+    let mortalWoundsDamage = 0;
+    if (devastatingWoundsBool) {
+        mortalWounds = getMortalWounds(woundingAttacks, ident);
+        woundingAttacks = woundingAttacks - mortalWounds;
+        mortalWoundsDamage = calculateMortalWoundsDamage(mortalWounds, ident);
     }
 
-    let woundsAttacks = calculateDamage(woundingAttacks, ident, mortalWounds);
+    let woundsAttacks = calculateDamage(woundingAttacks, ident);
 
 
-    finalWords(woundsAttacks, ident, mortalWounds);
+    finalWords(woundsAttacks, ident, mortalWoundsDamage);
 }
 
 /**
@@ -80,6 +85,22 @@ function calculateSuccesses(succRoll) {
  */
 function calculateFailure(succRoll) {
     return (succRoll - 1) / 6
+}
+
+/**
+ * Calculates amount of 6 within successfull dice rolls. wounds the attack does. (Or Lethal hits, or other stuff that only happens on 6es)
+ * How: We check the proportion of how many of the succesfull wounds are triggering ones. 
+ * 
+ * Example in wound roll: If we wound on a 4, have 3 Successfull wounds and do devastating Wounds on a 6, we calculate: 
+ * (1 / (7-4)) * 3 
+ * 1/3 * 3
+ * 1 Devastating Wound, because only one of them is (Statistically) a critical wound (with a 6).
+ * @param {Number} neededDiceValue The Value a dice needs to show, to count as "successfull" (for example: the BS)
+ * @param {Number} amountOfSuccessfullDice Amount of the dice, that (statistically) succeeded the roll.
+ * @returns {Number} Amount of 6es that we get statistically within the successfull rolls.
+ */
+function calculateOneOf6(neededDiceValue, amountOfSuccessfullDice) {
+    return ((1 / (7 - neededDiceValue)) * amountOfSuccessfullDice);
 }
 
 /**
@@ -114,11 +135,10 @@ function getHittingAttacks(ident) {
 }
 
 function getWounding(hits, ident) {
-    let strength = Number(document.getElementById('inStrength' + ident).value);
-    let toughness = Number(document.getElementById('inTough' + ident).value);
+
     let wundRollReroll = document.getElementById('woundReroll' + ident);
 
-    let woundRoll = Number(getWoundRoll(strength, toughness));
+    let woundRoll = Number(getWoundRoll(ident));
     let woundFactor;
 
     if (!wundRollReroll.checked) {
@@ -127,12 +147,19 @@ function getWounding(hits, ident) {
         woundFactor = Number(calculateSuccessWithRerolls(woundRoll));
     }
 
-    console.log(woundFactor);
-
     return hits * woundFactor;
 }
 
-function getWoundRoll(strength, toughness) {
+/**
+ * Calculates the Wound Roll
+ * @param {String} ident Wichtig um das richtige Feld abzufragen
+
+ * @returns {Number} Number to hit for successfull wound
+ */
+function getWoundRoll(ident) {
+    let strength = Number(document.getElementById('inStrength' + ident).value);
+    let toughness = Number(document.getElementById('inTough' + ident).value);
+
     if (strength == toughness) {
         return 4;
     } else if (strength > toughness) {
@@ -151,6 +178,53 @@ function getWoundRoll(strength, toughness) {
 };
 
 /**
+ * Gets amount on lethal hits
+ * @param {Number} hits Amount of successfull hitting dices
+ * @param {String} ident Identificator
+ * @returns 
+ */
+function getLethalHits(hits, ident) {
+    let hitRoll = Number(document.getElementById('inHitRoll' + ident).value);
+    return calculateOneOf6(hitRoll, hits);
+}
+
+
+/**
+ * Calculates amount of Devastating wounds the attack does.
+ * How: We check the proportion of how many of the succesfull wounds are devastating ones. 
+ * Example: If we wound on a 4, have 3 Successfull wounds and do devastating Wounds on a 6, we calculate: 
+ * (1 / (7-4)) * 3 
+ * 1/3 * 3
+ * 1 Devastating Wound, because only one of them is (Statistically) a critical wound (with a 6).
+ * @param {*} woundingAttacks 
+ * @param {*} ident 
+ * @param {*} antiValue 
+ * @returns 
+ */
+function getMortalWounds(woundingAttacks, ident, antiValue) {
+
+    let woundRoll = getWoundRoll(ident);
+
+    if (!antiValue) {
+        return calculateOneOf6(woundRoll, woundingAttacks)
+
+    } else {
+        if (antiValue < 0) { antiValue = antiValue * (-1) };
+        alert("Anti-X is not implemented now!")
+    }
+}
+
+/**
+ * Calculates amount of damage, the devastating wounds do
+ * @param {Number} mortalWounds 
+ * @returns {NUmber} Amount of damage
+ */
+function calculateMortalWoundsDamage(mortalWounds, ident) {
+    let woundCharachteristic = Number(document.getElementById('inWoundChar' + ident).value);
+    return Number(mortalWounds * woundCharachteristic);
+}
+
+/**
  * 
  * @param {Number} woundingAttacks 
  * @param {String} ident Identifier Number, can be empty
@@ -163,6 +237,7 @@ function calculateDamage(woundingAttacks, ident) {
     let woundCharachteristic = Number(document.getElementById('inWoundChar' + ident).value)
 
     let realSV = saveUnit + armourPenetration
+
     if (realSV > 6) {
         return [((woundingAttacks) * woundCharachteristic).toFixed(2), woundingAttacks.toFixed(2)]
     } else {
@@ -182,6 +257,12 @@ function getAP(ident) {
     return armourPenetration
 }
 
+/**
+ * Creating a nice text out of all the math back here
+ * @param {*} woundsAttacks 
+ * @param {*} ident 
+ * @param {*} mortalWounds 
+ */
 function finalWords(woundsAttacks, ident, mortalWounds) {
 
     if (typeof (woundsAttacks) == "string") { woundsAttacks = woundsAttacks.split(",") };
@@ -189,14 +270,14 @@ function finalWords(woundsAttacks, ident, mortalWounds) {
     let woundText = Number(Number(woundsAttacks[0]) + Number(mortalWounds)).toFixed(2) + ' Wunden,  ';
     let attacksText = 'in ' + woundsAttacks[1] + ' Angriffen.';
     let mortalWoundsText = "";
-    if (woundsAttacks[0] == 1) {
+    if (woundsAttacks[0] == 1 && mortalWounds == 0) {
         woundText = '1 Wunde am Ziel,  '
     }
     if (woundsAttacks[1] == 1) {
         attacksText = 'mit einem Angriff.'
     }
     if (mortalWounds) {
-        mortalWoundsText = ' Davon sind ' + mortalWounds + " tödliche Wunden."
+        mortalWoundsText = ' <br>Davon entstehen ' + Number(woundsAttacks[0]) + ' durch normale Angriffe, ' + Number(mortalWounds).toFixed(2) + ' durch tödliche Wunden.'
     }
 
     document.getElementById('resultTag' + ident).innerHTML = 'Der Angriff verursacht statistisch ' + woundText + attacksText + mortalWoundsText;
